65fd59327149134d2129775fc2f649a4c22af28d
henry-255 <mba.henry@lmu.edu.ng>
2024-12-27 06:33:04 +0100

    omo i just dey start

---
+++ app/Models/Candidate.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Candidate extends Model
+{
+    public function votes()
+    {
+        return $this->hasMany(Vote::class);
+    }
+
+}
